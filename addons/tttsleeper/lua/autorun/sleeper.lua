7087ce6997b2c53d8418f346183a3e6dbaefdc4f
mezzokoko <cptkeyes@gmx.de>
2015-10-18 05:59:44 +0200

    Fixed ttt_debug_preventwin

--- addons/tttsleeper/lua/autorun/sleeper.lua
+++ addons/tttsleeper/lua/autorun/sleeper.lua
@@ -9,25 +9,25 @@ local function sleeper()
     for _, v in pairs(player.GetAll()) do
         if v:Alive() and v:IsTraitor() then table.insert(alive_t, v) end
     end
-    
+
     if #alive_t == 0 and !sleeper_active then
         local target_pool = {}
         for _, ply in pairs(player.GetAll()) do
-            if not ply:IsTraitor() and not ply:IsDetective() and ply:Alive() and not ply:IsSpec() then table.insert(target_pool, ply)    end
+            if not ply:IsTraitor() and not ply:IsDetective() and ply:Alive() and not ply:IsSpec() then table.insert(target_pool, ply) end
         end
-        if #target_pool > 1 then 
+        if #target_pool > 1 then
             local ply = table.Random(target_pool)
             ply:SetRole(ROLE_TRAITOR)
-            
+
             ply:ChatPrint("You're the Sleeper Traitor! Go finish the job!")
-                       
+
             net.Start("TTT_Role")
             net.WriteUInt(ply:GetRole(), 2)
             net.Send(ply)
-            
+
             -- Hitman compatibility hook
             hook.Call("SleeperHitman", GAMEMODE, ply)
-			
+
             if GetConVar("ttt_sleeper_warning"):GetBool() then --imo warning the players of the sleeper traitor defeats the point of it, which is why it is disabled by default
                 for _, v in pairs( player.GetAll() ) do
                     if v != ply then
